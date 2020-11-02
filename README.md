ce64dcaaf4fd53953f785445658b408849b4ec5a
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-11-02 15:44:40 +0000

    Added bootstrap, refactors, readme improvement

--- README.md
+++ README.md
@@ -8,3 +8,17 @@
 
 2. Run `npm run install`
 3. Run `npm run start`
+
+## Information
+
+#### `npm run install`
+
+This command will install the required libraries
+
+#### `npm run start`
+
+This command will host the server at `localhost:8000`
+
+#### `.htaccess`
+
+This file is used to re-route all requests to `router.php` so we can route from that single file
--- router.php
+++ router.php
@@ -1,12 +1,15 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+// Import Twig
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
+// Set up Twig
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader);
 
+// Reroute to specific Twig page depending on requested route
 $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
