7b66be4562c30685d60763b35821d7ecadfe3841
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-11-03 15:08:43 +0000

    Rewrote helper

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,7 @@
-function scrollToFaq(event) {
+document.querySelector('#more-btn').addEventListener('click', (event) => {
     event.preventDefault();
 
     document
         .querySelector('.frequently-asked-questions')
         .scrollIntoView({ behavior: 'smooth' });
-}
-
-document.querySelector('#more-btn').addEventListener('click', scrollToFaq);
+});
