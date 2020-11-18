3eec34330f348c5fcf257237316942b04a37ca8e
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-11-18 12:53:35 +0000

    Basic landmark layout

--- src/php/detect.php
+++ src/php/detect.php
@@ -5,27 +5,26 @@ require __DIR__ . '/../php/db-connection.php';
 
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
-
 function detect_landmark($path)
 {
     // Create image reader and connect to database
-    $imageAnnotator = new ImageAnnotatorClient();
+    // $imageAnnotator = new ImageAnnotatorClient();
     $conn = openConnection();
 
     // Read image, submit to API and read landmark data response
-    $image = file_get_contents($path);
-    $response = $imageAnnotator->landmarkDetection($image);
-    $landmarks = $response->getLandmarkAnnotations();
+    // $image = file_get_contents($path);
+    // $response = $imageAnnotator->landmarkDetection($image);
+    // $landmarks = $response->getLandmarkAnnotations();
 
     // Close image reader connection
-    $imageAnnotator->close();
+    // $imageAnnotator->close();
 
-    // $landmarks = ['Spire of Dublin', 'Statue'];
+    $landmarks = ['Spire of Dublin', 'Statue'];
 
     foreach ($landmarks as $landmark) {
-        $name = $landmark->getDescription();
+        // $name = $landmark->getDescription();
 
-        // $name = $landmark;
+        $name = $landmark;
 
         $sql = 'SELECT * FROM landmarks WHERE landmark_name = ?';
 
@@ -37,7 +36,9 @@ function detect_landmark($path)
         $query->execute();
 
         $result = $query->get_result();
-        if ($result->num_rows === 0) continue;
+        if ($result->num_rows === 0) {
+            continue;
+        }
 
         $conn->close();
         return $result->fetch_row();
